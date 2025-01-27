00d749f160ba0f612a53717e8153dfb5095f1ea1
Antonio Acosta <antonio.acosta@intility.no>
2025-01-27 20:51:32 +0100

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Message } from "@intility/bifrost-react";
 
 function App() {
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <Message state="warning">
-                Coming soon: Dinner planner
-            </Message>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Message state="warning">Coming soon: Dinner planner</Message>
+    </div>
+  );
 }
 
 export default App;
--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,32 +1,33 @@
-import { Nav } from '@intility/bifrost-react';
-import App from '../App';
+import { Nav } from "@intility/bifrost-react";
+import App from "../App";
 
 const Navigation = () => {
-    return (
-        <Nav
-            // if you only supply a string to `logo` you'll get an "it" logo graphic
-            logo='Dinner planner'
-            hideBranding
-            side={
-                <div>
-                    <a href='/'>
-                        <Nav.Item>Home</Nav.Item>
-                    </a>
-                    <a href='/profile'>
-                        <Nav.Item>Profile</Nav.Item>
-                    </a>
-                </div>
-            }
-            top={
-                <>
-                    <a href='/profile' title='User profile'>
-                        <Nav.Item />
-                    </a>
-                </>
-            }>
-            <App />
-        </Nav>
-    );
+  return (
+    <Nav
+      // if you only supply a string to `logo` you'll get an "it" logo graphic
+      logo="Dinner planner"
+      hideBranding
+      side={
+        <div>
+          <a href="/">
+            <Nav.Item>Home</Nav.Item>
+          </a>
+          <a href="/profile">
+            <Nav.Item>Profile</Nav.Item>
+          </a>
+        </div>
+      }
+      top={
+        <>
+          <a href="/profile" title="User profile">
+            <Nav.Item />
+          </a>
+        </>
+      }
+    >
+      <App />
+    </Nav>
+  );
 };
 
 export default Navigation;
