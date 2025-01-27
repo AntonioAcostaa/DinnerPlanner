87c6f53d78fc25bc76f5a98eaa81622e72c5f78c
Antonio Acosta <antonio.acosta@intility.no>
2025-01-27 20:53:07 +0100

    Fix filepath

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@intility/bifrost-react/bifrost-app.css";
-import Navigation from "./navigation/navigation.tsx";
+import Navigation from "./navigation/Navigation";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
